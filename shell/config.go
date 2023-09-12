e25457e805c87b0b2602d68d18ad856b9b61e116
SedationH <sedationh@gmail.com>
2023-09-12 23:39:40 +0800

    优化命令行调用方式

--- shell/config.go
+++ shell/config.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
 
@@ -18,9 +19,19 @@ func InitConfig() {
 	viper.SetConfigType("yml")
 	viper.AddConfigPath("./")
 
-	viper.SetDefault("Role", "admin")
-	viper.SetDefault("Port", "8899")
-	viper.SetDefault("Host", "127.0.0.1")
+	var Role string
+	var Host string
+	var Port string
+
+	pflag.StringVarP(&Role, "role", "r", "admin", "角色")
+	pflag.StringVarP(&Port, "port", "p", "8899", "端口")
+	pflag.StringVarP(&Host, "host", "h", "127.0.0.1", "主机，非 admin 角色需要指定 host")
+	pflag.Parse()
+	fmt.Println(Role, Port, Host)
+
+	viper.SetDefault("Role", Role)
+	viper.SetDefault("Port", Port)
+	viper.SetDefault("Host", Host)
 
 	_ = viper.ReadInConfig()
 
